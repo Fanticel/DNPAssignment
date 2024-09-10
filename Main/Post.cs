26b7ac7f9c57a90c0741a91bd2f06c7838c3f0ed
Fanticel <duzywiewior@tlen.pl>
2024-09-10 12:21:49 +0200

    InitCommit

---
+++ Main/Post.cs
@@ -0,0 +1,41 @@
+﻿namespace Main;
+
+public class Post {
+    private int Likes;
+    private int Dislikes;
+    private int PostId;
+    private User Op;
+    private List<Comment> Comments;
+    public string PostTitle { get; set; }
+    public string PostBody { get; set; }
+    public Post(User Op, string postTitle, string postBody) {
+        PostId = 1;
+        Likes = 0;
+        Dislikes = 0;
+        this.Op = Op;
+        PostTitle = postTitle;
+        PostBody = postBody;
+        Comments = new List<Comment>();
+    }
+
+    public void PostComment(User CommentOp, string content) {
+        Comments.Add(new Comment(CommentOp, content));
+    }
+    
+    public override string ToString() {
+        return $"OP:{Op.UserName}\n___{PostTitle}___\n\t{Likes} | {Dislikes}";
+    }
+    public string FullToString() {
+        string ans = $"OP:{Op.UserName}\n___{PostTitle}___\n{PostBody}\n\t{Likes} | {Dislikes}\n";
+        foreach (Comment com in Comments) {
+            ans += com + "\n";
+        }
+        return ans;
+    }
+    public void Like() {
+        Likes++;
+    }
+    public void Dislike() {
+        Dislikes++;
+    }
+}
---
+++ Main/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Main;
+
+public class User {
+    private int UserId;
+    public string UserName { get; set; }
+    public string UserPass { get; set; }
+
+    public User(string userName, string userPass) {
+        UserId = 1;
+        UserName = userName;
+        UserPass = userPass;
+    }
+}
